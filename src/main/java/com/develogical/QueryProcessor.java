33e3acb268fcdd634c1f4e3b54ad08248ba3fac2
Chris Battarbee <chris@chrisbattarbee.com>
2020-01-21 11:28:47 +0000

    team name

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,6 +6,9 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("what is your team name")) {
             return "Dynamic_Duo";
         }
+        if (query.toLowerCase().contains("what is") && query.toLowerCase().contains("plus")) {
+            return Integer.toString(Integer.parseInt(query.toLowerCase().split(" ")[3]) + Integer.parseInt(query.toLowerCase().split(" ")[5]));
+        }
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
